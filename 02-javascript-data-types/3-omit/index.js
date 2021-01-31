56e068f99d870c9710c62316385c58286cd8dde8
Dementiy <Dementiy@yandex.ru>
2021-01-31 20:45:05 +0300

    Добавлено решение для omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,11 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+    let new_obj = {};
+    for (let field in obj) {
+        if (!fields.includes(field)) {
+            new_obj[field] = obj[field];
+        }
+    }
+    return new_obj;
 };
