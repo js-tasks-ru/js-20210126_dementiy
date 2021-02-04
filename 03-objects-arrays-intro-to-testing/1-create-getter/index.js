f2cdfa804e486d3b366ff152ffa692041746e91b
Dementiy <Dementiy@yandex.ru>
2021-02-04 18:56:10 +0300

    Компоненты пути определяются один раз

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,12 +4,11 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const pathParts = path.split(".");
     return (obj) => {
-        for (const key of path.split(".")) {
+        for (const key of pathParts) {
             obj = obj[key];
-            if (obj === undefined) {
-                return undefined;
-            }
+            if (obj === undefined) return;
         }
         return obj;
     }
