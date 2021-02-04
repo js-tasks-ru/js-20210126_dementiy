db92c3885f75f309dab70e23c533d649fdd51f96
Dementiy <Dementiy@yandex.ru>
2021-02-04 18:58:51 +0300

    Упрощено условие

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,9 +4,7 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-    if (obj === undefined) {
-        return undefined;
-    }
+    if (obj === undefined) return;
 
     const newObj = {};
     Object.entries(obj).forEach(([k,v]) => {
